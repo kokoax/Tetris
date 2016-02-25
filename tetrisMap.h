275921cec2eafc0708621c9bb93e2fc764314af8
Yoshitomo Tokoi <yomo4808@gmail.com>
2016-02-26 00:19:15 +0900

    パターンの移動処理のインターフェイスの定義

--- tetrisMap.h
+++ tetrisMap.h
@@ -2,13 +2,19 @@
 
 #include "tetris.h"
 #include "tetrisPattern.h"
+#define MAP_HIGH 20
+#define MAP_WIDTH 10
 
 class tetrisMap {
   private:
-    //char map[MAP_HIGH][MAP_WIDTH];
+    char map[MAP_HIGH][MAP_WIDTH];
+
+    void hidePattern( const tetrisPattern ctl );
+    void appearPattern( const tetrisPattern ctl );
+    void printMap( void );
 
   public:
     tetrisMap( void );
-    void putMap( tetrisPattern nowPattern );
+    void movePattern( tetrisPattern *nowPattern );
 };
 
