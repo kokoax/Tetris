eb9d0dd9c129623ccbcdd6c31fe955a6718aa1e7
Yoshitomo Tokoi <yomo4808@gmail.com>
2016-02-26 19:04:38 +0900

    tetrisRunでtetrisMapも動かすように変更

--- tetrisRun.cpp
+++ tetrisRun.cpp
@@ -2,9 +2,11 @@
 //2016-02-25
 #include "tetrisRun.h"
 #include "tetrisPattern.h"
+#include "tetrisMap.h"
 
 tetrisRun::tetrisRun( void ){
   tetrisPattern Pattern;
+  tetrisMap     Map;
   //auto result = async( launch::async, [] { return long_calc(); } );
   while( 1 ){
     /*
