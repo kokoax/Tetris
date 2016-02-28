d203784a13b0bd643012043c58286a2bfd5f61a7
Yoshitomo Tokoi <yomo4808@gmail.com>
2016-02-28 21:26:33 +0900

    落下しているブロックが接地したのを調べる機能を追加

--- tetrisMap.cpp
+++ tetrisMap.cpp
@@ -11,7 +11,8 @@ void tetrisMap::initMap( void ){
 }
 
 void tetrisMap::printMap( void ){
-  fprintf( stderr, "\e[2J" );    // 画面クリア
+  fprintf( stderr, "\e[1J" );    // 画面クリア
+  map[MAP_HIGH-3][MAP_WIDTH-3] = '#';
   for( int i = 1; i <= MAP_HIGH; i++ ){
     //先頭に行数を表示しているため左に二個ずらしている
     for( int j = 1; j <= MAP_WIDTH; j++ ){
@@ -41,21 +42,34 @@ tetrisMap::tetrisMap( void ){
   printMap();
 }
 
-void tetrisMap::movePatternDown( PATTERN_RETENTION *nowPattern ){
+int tetrisMap::movePatternDown( PATTERN_RETENTION *nowPattern ){
+  //if( nowPattern->y < MAP_HIGH - (int)nowPattern->pattern.size() -1 ){
   hidePattern( *nowPattern );
   nowPattern->y++;
+  if( checkPenetrate( *nowPattern ) != true ){
+    return false;
+  }
   appearPattern( *nowPattern );
+  return true;
 }
 
 void tetrisMap::movePatternRight( PATTERN_RETENTION *nowPattern ){
+  //if( nowPattern->x < MAP_WIDTH - (int)nowPattern->pattern[0].size() -1 ){
   hidePattern( *nowPattern );
   nowPattern->x++;
+  if( checkPenetrate( *nowPattern ) != true ){
+    nowPattern->x--;
+  }
   appearPattern( *nowPattern );
 }
 
 void tetrisMap::movePatternLeft( PATTERN_RETENTION *nowPattern ){
+  //if( nowPattern->x > 1 ){
   hidePattern( *nowPattern );
   nowPattern->x--;
+  if( checkPenetrate( *nowPattern ) != true ){
+    nowPattern->x++;
+  }
   appearPattern( *nowPattern );
 }
 
@@ -83,8 +97,30 @@ void tetrisMap::appearPattern( const PATTERN_RETENTION ctl ){
   }
 }
 
+int tetrisMap::checkPenetrate( PATTERN_RETENTION nowPattern ){
+  if ( nowPattern.y < 1 ){
+    return UP;
+  } else if( nowPattern.y > ( MAP_HIGH  - (int)nowPattern.pattern.size()    -1 ) ){
+    return DOWN;
+  } else if( nowPattern.x < 1 ){
+    return LEFT;
+  } else if( nowPattern.x > ( MAP_WIDTH - (int)nowPattern.pattern[0].size() -1 ) ){
+    return RIGHT;
+  } else {
+    for( int i = 0; i < (int)nowPattern.pattern.size(); i++ ){
+      if( ( map[nowPattern.y + (int)nowPattern.pattern.size() -2][i+nowPattern.x-1] == '#' ) &&
+          ( nowPattern.pattern[(int)nowPattern.pattern.size() -1][i] =='#' ) ){
+        return DOWN;
+      }
+    }
+  }
+  return true;
+}
+
 void tetrisMap::turnPatternRight( PATTERN_RETENTION *nowPattern ){
   PATTERN_RETENTION work;
+  work.x = nowPattern->x;
+  work.y = nowPattern->y;
 
   hidePattern( *nowPattern );
 
@@ -99,9 +135,11 @@ void tetrisMap::turnPatternRight( PATTERN_RETENTION *nowPattern ){
     }
   }
 
-  nowPattern->pattern = work.pattern;
+  if( checkPenetrate( work ) == true ){
+    nowPattern->pattern = work.pattern;
+  }
 
-  appearPattern( *nowPattern );
+    appearPattern( *nowPattern );
 }
 
 void tetrisMap::test( void ){
